b8249170243af8d0541d885e8462874e50973347
Calamity-chain <78163718+Calamity-chain@users.noreply.github.com>
2021-04-11 19:48:30 +0200

    Imported React Bootstrap

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import Button from 'react-bootstrap/Button';
 import { Card } from 'react-bootstrap';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import './movie-card.scss';
 
@@ -12,22 +14,36 @@ export class MovieCard extends React.Component {
     // connected to your database via the movies endpoint of your API
     const { movie, onClick } = this.props;
 
+
     return (
-      <Card
-        onClick={() => onClick(movie)}
-        border='danger'
-        style={{ width: '200', height: 'auto' }}
-      >
-        <Card.Header>{movie.Title}</Card.Header>
-        <img
-          className='movie-poster'
-          src={movie.imagePath}
-          alt='movie poster'
-        />
+      <Card>
+        <Card.Img variant="top" src={movie.ImagePath} />
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Text>{movie.Description}</Card.Text>
+          <Button onClick={() => onClick(movie)} variant="link">Open</Button>
+        </Card.Body>
       </Card>
     );
   }
 }
+
+//     return (
+//       <Card
+//         onClick={() => onClick(movie)}
+//         border='danger'
+//         style={{ width: '200', height: 'auto' }}
+//       >
+//         <Card.Header>{movie.Title}</Card.Header>
+//         <img
+//           className='movie-poster'
+//           src={movie.imagePath}
+//           alt='movie poster'
+//         />
+//       </Card>
+//     );
+//   }
+// }
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
